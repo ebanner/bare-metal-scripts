2b4a93023ad4e6314b75d7c6cb2149f15f08eca9
Edward Banner <edward.banner@gmail.com>
2026-08-17 12:37:42 -0400

    Get sleep working

--- sleep/boot.asm
+++ sleep/boot.asm
@@ -8,6 +8,15 @@ _start:
     ; Set up stack pointer
     mov esp, 0x7c00
 
+    ; Read in one sector of data from disk
+    mov ah, 0x02   ; BIOS function for reading from disk
+    mov al, 1      ; Read 1 sector (512 byte chunk)
+    mov ch, 0      ; Cylinder number (which 3D ring)
+    mov cl, 2      ; Sector number (which slice of the pie [ring])
+    mov dh, 0      ; Head number (which level of the 3D ring [depth])
+    mov bx, 0x7e00 ; Buffer to store read data
+    int 0x13       ; BIOS interrupt for disk operations
+
 pm_entry:
     ; --- Enter protected mode ---
     cli
--- sleep/init.c
+++ sleep/init.c
@@ -8,25 +8,54 @@ static inline unsigned char inb(unsigned short port) {
     return ret;
 }
 
+int mvaddch(int y, int x, const char ch) {
+  volatile unsigned short *vga = (volatile unsigned short *)0xB8000;
+
+  int offset = 80*y + x;
+  vga[offset] = 0x0F00 | ch; /* 0x0F00 is white on black */
+
+  return 0;
+}
+
+static unsigned int CURSOR = 0;
+
+void putchar(const char c) {
+  int y = CURSOR / 80;
+  int x = CURSOR % 80;
+
+  mvaddch(y, x, c);
+
+  CURSOR += 1;
+}
+
 int sleep() {
-    unsigned int ms = 10000;
-    unsigned short divisor = (1193182 * ms) / 1000; // crude, only works for coarse values
+    unsigned int sec = 10;
+    unsigned int divisor = 1193182 * sec; // crude, only works for coarse values
 
-    /*
-     * Set the PIC to count down
-     *
-    */
-    outb(0x43, 0xB6);  // channel 2, lobyte/hibyte, mode 3 (square wave)
-    outb(0x42, divisor & 0xFF);         // low byte -> channel 2 data port
-    outb(0x42, (divisor >> 8) & 0xFF);  // high byte -> channel 2 data port
+    while (divisor > 0) {
+        /*
+        * Set the PIC to count down
+        *
+        */
+        outb(0x61, (inb(0x61) & ~0x02) | 0x01);  /* gate on, speaker off */
+        outb(0x43, 0xB0);                        /* channel 2, mode 0 */
+        outb(0x42, 65535 & 0xFF);
+        outb(0x42, (65535 >> 8) & 0xFF);
 
-    /*
-     * Spin until PIT output pin goes high (channel 0 status)
-     *
-     */
-    while (1) {
-        unsigned char status = inb(0x61);
-        if (status & 0x20)
-            break;
+        /*
+        * Spin until PIT output pin goes high (channel 0 status)
+        *
+        */
+        while (1) {
+            unsigned char status = inb(0x61);
+            if (status & 0x20)
+                break;
+        }
+
+        putchar('x');
+
+        unsigned int chunk = divisor > 65535 ? 65535 : divisor;
+
+        divisor -= chunk;
     }
 }
